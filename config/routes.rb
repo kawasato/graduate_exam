60e97a706092c194dc72a367bbcb49f26460305e
satomi <satomi.kawasaki0607@gmail.com>
2019-01-12 16:09:12 +0900

    _form.html.erbの実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do 
   root to: 'tops#index'
   devise_for :users
-  resources :shops
+  resources :shops do
+    collection do
+      post:confirm
+    end
+  end
 
 end
