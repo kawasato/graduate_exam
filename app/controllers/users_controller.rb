9affa31d1b4269e88c40d1b8c2f512bf7c6c2254
satomi <satomi.kawasaki0607@gmail.com>
2019-01-14 22:23:41 +0900

    favorite、show、edit画面実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,9 @@ class UsersController < ApplicationController
     def show
         @user = User.find(params[:id])
     end
+
+    def favorite
+        @user = User.find(params[:id])
+        @favorites_shops = @user.favorites
+    end
 end
